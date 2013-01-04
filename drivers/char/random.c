6e391c0086e652aa6f3a68687d735d18b7c9a5a4
imoseyon <imoseyon@gmail.com>
2013-01-04 13:05:20 -0800

    random: prevent add_input from doing anything
    Change-Id: I17e734057ba567f084ecd6eaa3ef282720608d66
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -783,16 +783,7 @@ out:
 void add_input_randomness(unsigned int type, unsigned int code,
 				 unsigned int value)
 {
-	static unsigned char last_value;
-
-	/* ignore autorepeat and the like */
-	if (value == last_value)
-		return;
-
-	last_value = value;
-	add_timer_randomness(&input_timer_state,
-			     (type << 4) ^ code ^ (code >> 4) ^ value);
-	trace_add_input_randomness(ENTROPY_BITS(&input_pool));
+	return;
 }
 EXPORT_SYMBOL_GPL(add_input_randomness);
 
