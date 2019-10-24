47dc09c119918ad2990ffc9bf586c77f3eb89d2f
ananjaser1211 <ananjaser@gmail.com>
2019-10-24 20:18:51 +0400

    wl_blocker: add more wakelocks to the default block list

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,8 +16,8 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"wlan_rx_wake;wlan_wake;wlan_ctrl_wake;NETLINK;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;mmc0_detect;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock"
+#define LIST_WL_DEFAULT			"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source"
 
-#define LENGTH_LIST_WL				2048
+#define LENGTH_LIST_WL			2048
 #define LENGTH_LIST_WL_DEFAULT		255
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
